5bd361febe80154bd905afeb4a669d4a5d6f24ed
Nicolás Rincón Mújica <nicolas.rm540@gmail.com>
2020-07-27 22:08:54 -0500

    Eventos o Mensajes Para Todos Los Usuarios Utilizando Servicios REST/SOCKETS, Mensajes Privados Para Una Conexion Especifica Utilizando SOCKETS

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
 
 
 const router = Router();
@@ -11,9 +12,21 @@ router.get('/mensajes', (req: Request, res: Response) => {
     });
 });
 
+
+// Servicio REST Para Enviar Mensajes A Todos
 router.post('/mensajes', (req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
+
+
+    const server = Server.instance;
+    const payload = {
+        cuerpo,
+        de
+    }
+    // Envia a Todas Las Salas
+    server.io.emit('mensaje-nuevo', payload);
+
     res.json({
         POST: 'Desde Mensajes',
         cuerpo,
@@ -21,10 +34,28 @@ router.post('/mensajes', (req: Request, res: Response) => {
     });
 });
 
+// Servicio REST Para Enviar Mensajes Privados
 router.post('/mensajes/:id', (req: Request, res: Response) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
+
+    // Misma Instancia, Por El Patron Singleton
+    const server = Server.instance;
+    // in: Conectar Con Alguien En Un Canal / Sala En Especifica
+    // emit: Emitir Mensajes / Eventos
+
+    // Ejemplo
+    const payload = {
+        de,
+        cuerpo
+    }
+    // Envia a Todas Las Salas
+    // server.io.emit('mensaje-privado', payload);
+
+    // Envia a Una Sala En Especifica
+    server.io.in(id).emit('mensaje-privado', payload);
+
 
     res.json({
         POST: 'Desde Mensajes + Parametro.',
